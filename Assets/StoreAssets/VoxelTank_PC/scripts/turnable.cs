9c8dbbcaf0fbedaafd4a289a08006782530f9b1c
Sansrf <110791650+Sansrf@users.noreply.github.com>
2023-07-15 20:29:51 -0700

    Dictatorito SadStachio

---
+++ Assets/StoreAssets/VoxelTank_PC/scripts/turnable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnable : MonoBehaviour {
+
+    public float spd=4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.up * Time.deltaTime*spd);
+    }
+}
